8d488dc85d693341c98157efff50bc2c138cf281
gabrielluizep <gabrielluizep@hotmail.com>
2023-12-08 02:08:24 +0000

    inicializado projetos do servidor e dos dispostivos

---
+++ entrega-4/server/src/test/java/eng/tele/std/StdApplicationTests.java
@@ -0,0 +1,13 @@
+package eng.tele.std;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
